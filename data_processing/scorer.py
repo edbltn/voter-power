b505358870a4f1a0594e7b111ffa09c21d0ff8f0
edbltn <edbltn@gmail.com>
2020-10-27 11:55:20 -0400

    [Fix] scorer to match 2020 data

--- data_processing/scorer.py
+++ data_processing/scorer.py
@@ -5,20 +5,24 @@ import os
 import math
 
 class Scorer:
-    def __init__(self, data_dir_name):
+
+    def __init__(self, data_dir_name: str=None):
         data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),data_dir_name)
-        scores_df = pd.read_csv(os.path.join(data_dir_name,'scores.csv')) 
-        scores_df = scores_df[scores_df.congress >= 107]
+        self.scores_df = pd.read_csv(os.path.join(data_dir_name,'scores.csv'))
+        self.scores_df = self.scores_df[self.scores_df.congress >= 107]
 
         self.mean_d_scores = defaultdict(lambda: np.zeros(2))
         self.mean_r_scores = defaultdict(lambda: np.zeros(2))
         self.mean_scores = defaultdict(lambda: np.zeros(2))
+
+        self.official_scores = {}
+        self.init_scores()
+
+    def init_scores(self):
         d_total = defaultdict(lambda: 0)
         r_total = defaultdict(lambda: 0)
         total = defaultdict(lambda: 0)
-
-        self.official_scores = {}
-        for index, row in scores_df.iterrows():
+        for index, row in self.scores_df.iterrows():
             # Make sure score is valid
             score = np.array([float(row['nominate_dim1']), float(row['nominate_dim2'])])
             if math.isnan(score[0]) or math.isnan(score[1]):
@@ -33,10 +37,13 @@ class Scorer:
                 party = 'D'
 
             # Update member score
-            if row['congress'] == 115:
+            if row['congress'] >= 102:
                 official_id = row['bioname'].split(',')[0].lower() + '_' + state + '_' + party
                 self.official_scores[official_id] = score
                 self.official_scores[row['icpsr']] = score
+            else:
+                continue
+
             # Update estimate scores
             if row['party_code'] == 200:
                 r_total[state] += 1
@@ -46,6 +53,7 @@ class Scorer:
                 d_total[state] += 1
                 delta = score - self.mean_d_scores[state]
                 self.mean_d_scores[state] += delta / d_total[state]
+
             total[state] += 1
             delta = score - self.mean_scores[state]
             self.mean_scores[state] += delta / total[state]
@@ -54,8 +62,14 @@ class Scorer:
         name, party = politician
         names = name.split(' ')
         state = code[:2]
+        # Look for each combination of last name tokens in order
         for i in reversed(range(len(names))):
             official_id = ' '.join(names[i:]).lower() + '_' + state + '_' + party
+            if official_id.startswith('trump'):
+                # Per https://projects.fivethirtyeight.com/congress-trump-score/, Rick Scott is the "Trumpiest Senator"
+                official_id = 'scott_FL_R'
+            elif official_id.startswith('biden'):
+                official_id = 'biden_DE_D'
             if official_id in self.official_scores:
                 return self.official_scores[official_id]
 
